28c2162fb112ed8683b921f13d22e40df26d2750
Christine Faye Valle <christinefayevalle912@gmail.com>
2018-11-21 14:58:25 +0800

    last na sana

---
+++ api/models/Report.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+    attribute:{
+      id:{
+        type:'number'
+      },
+      category:{
+        type:'string'
+      },
+      message:{
+        type:'text'
+      },
+      name:{
+        type:'string'
+      }
+  },
+};
+
