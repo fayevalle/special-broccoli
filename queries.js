b13090fdedd5b84c2f4a921ebdf6d0e9d5d7331b
Christine Faye Valle <christinefayevalle912@gmail.com>
2018-11-23 01:30:27 +0800

    makakapag add naaaaaa

--- queries.js
+++ queries.js
@@ -39,8 +39,8 @@ function getAllPuppies(req, res, next) {
 }
 function createPuppy(req, res, next) {
   req.body.age = parseInt(req.body.age);
-  db.none('insert into pups(name, breed, age, sex)' +
-      'values(${name}, ${breed}, ${age}, ${sex})',
+  db.none('insert into report_tbls(category, description)' +
+      'values(${category}, ${message})',
     req.body)
     .then(function () {
       res.status(200)
