9e6dd9681dc5e0e52a3ed8e9c1a390153817a13c
seph <seph@bluestellas-Air.localhost>
2018-11-23 00:15:44 +0800

    yes

--- queries.js
+++ queries.js
@@ -7,6 +7,8 @@ var options = {
 
 var pgp = require('pg-promise')(options);
 var connectionString = 'postgres://wgldewicamedev:8d6d9755d8e6ceba962b9f61b8a4e4772218756930a3c4b2de8493f0428e0b8d@ec2-54-243-147-183.compute-1.amazonaws.com:5432/d68rvuaemp326g';
+
+pgp.pg.defaults.ssl = true;
 var db = pgp(connectionString);
 
 // add query functions
