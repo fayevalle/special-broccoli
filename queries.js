e2386ce8027bfd651fd159c46a6ec2620a794fb9
seph <seph@bluestellas-Air.localhost>
2018-11-22 23:58:39 +0800

    bla

---
+++ queries.js
@@ -0,0 +1,37 @@
+var promise = require('bluebird');
+
+var options = {
+  // Initialization Options
+  promiseLib: promise
+};
+
+var pgp = require('pg-promise')(options);
+var connectionString = 'postgres://wgldewicamedev:8d6d9755d8e6ceba962b9f61b8a4e4772218756930a3c4b2de8493f0428e0b8d@ec2-54-243-147-183.compute-1.amazonaws.com:5432/d68rvuaemp326g';
+var db = pgp(connectionString);
+
+// add query functions
+
+module.exports = {
+  getAllPuppies: getAllPuppies
+//  getSinglePuppy: getSinglePuppy,
+ // createPuppy: createPuppy,
+ // updatePuppy: updatePuppy,
+  //removePuppy: removePuppy
+};
+
+
+
+function getAllPuppies(req, res, next) {
+  db.any('select * from transactions_tbl')
+    .then(function (data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Retrieved ALL puppies'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
