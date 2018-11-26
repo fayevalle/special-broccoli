f230e155d5988bc334155d78edd1085b6f6422d7
Christine Faye Valle <christinefayevalle912@gmail.com>
2018-11-27 00:00:07 +0800

    hhmm.

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ var app = express();
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
 });
 
 var db = require('../queries');
